025014bf835495b8f5848ecc76483f5d46efdcf8
LinMinFan <cvse00566@gmail.com>
2022-07-25 19:48:18 +0800

    [changes] 版型重製一頁式 移除ifram改以ajax取代 使用套件及動畫美化

---
+++ js/main.js
@@ -0,0 +1,102 @@
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
+
+function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
+}
+fade($('.quoteLoop > .quote').first());
+
+
+/*----------------------------------------------------*/
+/* Navigation
+------------------------------------------------------ */
+
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 300) {
+        $('.main_nav').addClass('sticky');
+    } else {
+        $('.main_nav').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.main_nav').removeClass('open-nav');
+    } else {
+        $('.main_nav').addClass('open-nav');
+    }
+});
+
+$('.main_nav li a').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_nav').removeClass('open-nav');
+    }
+});
+
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+$(document).ready(function($) {
+
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+
+	});
+    
+    $(window).scroll(function(){
+        //console.log($("html,body").scrollTop())
+        let nowscroll = $(this).scrollTop()
+        //console.log(nowscroll)
+
+        //About Section
+        let abhd=$('#about').offset().top
+        //console.log(abhd)
+        if (nowscroll >= (abhd / 2)) {
+            $('#about').addClass("aboutshow");
+        }
+        
+        //Skills Section
+        let skhd=$('#skills').offset().top
+        //console.log(skhd)
+        if (nowscroll >= (skhd / 1.3)) {
+            $('#skillbox1').addClass("skillshow");
+            $('#skillbox2').addClass("skillshow");
+            $('#skillbox3').addClass("skillshow");
+            $('#skillbox4').addClass("skillshow");
+        }
+    })
+});
+
+/*----------------------------------------------------*/
+/* heading
+------------------------------------------------------ */
+
+$('#home').load("./home.html");
+$('#about').load("./about.html");
+$('#skills').load("./skills.html");
+$('#portfolio').load("./portfolio.html");
+$('#contact').load("./contact.html");
+
+
+
+
+
+TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
